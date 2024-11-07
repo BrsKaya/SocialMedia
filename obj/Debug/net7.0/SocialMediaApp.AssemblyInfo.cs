2655c73d3ab076940c29c620feca9caf2bbf2684
Sena <150042935+senademirbas@users.noreply.github.com>
2024-11-08 00:31:00 +0300

    change

--- obj/Debug/net7.0/SocialMediaApp.AssemblyInfo.cs
+++ obj/Debug/net7.0/SocialMediaApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SocialMediaApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f98a0463bcf372b0cc320af1fb211c583f165214")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dbfc99388984b38289024ba9ce27f2a5af179d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("SocialMediaApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SocialMediaApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
